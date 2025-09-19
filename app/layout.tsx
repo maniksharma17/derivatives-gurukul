61aab451e5873f4b30883d868db0fd64b70f7edc
Manik Sharma <maniksharma.rke@gmail.com>
2025-09-19 18:20:14 +0530

    pixel code added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
+import Image from 'next/image';
+import Script from 'next/script';
 
 const poppins = Poppins({
   subsets: ['latin'],
-  weight: ['300', '400', '500', '700'], 
+  weight: ['300', '400', '500', '700'],
   variable: '--font-poppins',
 });
 
@@ -29,7 +31,35 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${poppins.variable}`}>
-      <body>{children}</body>
+      <body>
+        {/* Facebook Pixel Script */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '702613076172667');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <Image
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            alt=''
+            src="https://www.facebook.com/tr?id=702613076172667&ev=PageView&noscript=1"
+          />
+        </noscript>
+
+        {children}
+      </body>
     </html>
   );
 }
